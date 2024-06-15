e1cad9845eff256871bf3b882481641c2159f9af
numilani <numilani@protonmail.com>
2024-06-15 00:26:54 -0400

    1.21 first build

--- src/main/java/me/numilani/loreclues/LoreClues.java
+++ src/main/java/me/numilani/loreclues/LoreClues.java
@@ -1,17 +1,25 @@
 package me.numilani.loreclues;
 
+import cloud.commandframework.annotations.AnnotationParser;
+import cloud.commandframework.execution.CommandExecutionCoordinator;
+import cloud.commandframework.meta.SimpleCommandMeta;
+import cloud.commandframework.paper.PaperCommandManager;
 import com.bergerkiller.bukkit.common.cloud.CloudSimpleHandler;
 import com.bergerkiller.bukkit.common.config.FileConfiguration;
 import me.numilani.loreclues.commands.ClueCommands;
 import me.numilani.loreclues.data.IDataSourceConnector;
 import me.numilani.loreclues.data.SqliteDataSourceConnector;
 import me.numilani.loreclues.listeners.HintListeners;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.SQLException;
+import java.util.function.Function;
 
 public final class LoreClues extends JavaPlugin {
-    public CloudSimpleHandler cmdHandler = new CloudSimpleHandler();
+//    public CloudSimpleHandler cmdHandler = new CloudSimpleHandler();
+    public PaperCommandManager<CommandSender> cmdHandler;
+    public AnnotationParser<CommandSender> cmdParser;
     public IDataSourceConnector dataSource;
 
     @Override
@@ -32,8 +40,12 @@ public final class LoreClues extends JavaPlugin {
             throw new RuntimeException(e);
         }
 
-        cmdHandler.enable(this);
-        cmdHandler.getParser().parse(new ClueCommands(this));
+        try {
+            cmdHandler = new PaperCommandManager<>(this, CommandExecutionCoordinator.simpleCoordinator(), Function.identity(), Function.identity());
+            cmdParser = new AnnotationParser<>(cmdHandler, CommandSender.class, parserParameters -> SimpleCommandMeta.empty());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
